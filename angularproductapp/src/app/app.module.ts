3c1365381fbdaae40e6675e71b3e312ff69d3c15
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-03-17 16:43:59 +0200

    added Top bar component

--- angularproductapp/src/app/app.module.ts
+++ angularproductapp/src/app/app.module.ts
@@ -1,23 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AppComponent } from "./app.component";
+import { ProductListComponent } from "./product-list/product-list.component";
 
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { CommonModule } from '@angular/common';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
+import { TopBarComponent } from "./top-bar/top-bar.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    TopBarComponent
   ],
-  imports: [
-    BrowserModule,
-    CommonModule
-
-  ],
+  imports: [BrowserModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
