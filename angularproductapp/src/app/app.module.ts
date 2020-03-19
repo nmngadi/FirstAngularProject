28d8e0f199d4e9c70d8edbb6f8d5c270f69b7b91
Nhlelo Mngadi <NMngadi@jhb.dvt.co.za>
2020-03-19 09:47:05 +0200

    add references on app module

--- angularproductapp/src/app/app.module.ts
+++ angularproductapp/src/app/app.module.ts
@@ -1,21 +1,30 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { AppComponent } from "./app.component";
-import { ProductListComponent } from "./product-list/product-list.component";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
-import { TopBarComponent } from "./top-bar/top-bar.component";
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+    ])
+  ],
   declarations: [
     AppComponent,
+    TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    TopBarComponent
+    ProductDetailsComponent
   ],
-  imports: [BrowserModule, CommonModule],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule {}
+export class AppModule { }
